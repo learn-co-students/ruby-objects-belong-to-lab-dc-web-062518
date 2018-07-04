7da1a9657640a3f0a606d05c53b6543b06613ca4
github username <github email address>
2018-07-04 11:32:43 -0400

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,16 @@
+require 'pry'
+
 class Author
 
   attr_accessor :name
 
+  def initialize(name)
+    @name = name
+  end
+
 end
+
+angela = Author.new("Angela Davis")
+
+# binding.pry
+# puts "hey"
--- lib/post.rb
+++ lib/post.rb
@@ -1,6 +1,20 @@
-
+require 'pry'
 class Post
 
   attr_accessor :title, :author
 
-end 
+  def initialize(title, author)
+    @title = title
+    @author = author
+  end
+
+end
+
+angela = Author.new("Angela Davis")
+women_book = Post.new("Women, Race, Class", "Angela Davis")
+
+women_book.author = angela
+
+
+# binding.pry
+# puts "hey"
